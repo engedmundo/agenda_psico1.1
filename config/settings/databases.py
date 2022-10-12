33be2ce562ea8e56080b112074c4283006622e98
Edmundo Abreu e Lima <engedmundo@gmail.com>
2022-10-12 20:35:06 -0300

    adjust settings

--- config/settings/databases.py
+++ config/settings/databases.py
@@ -4,8 +4,10 @@ import os
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("DATABASE_ENGINE"),
-        "NAME": os.environ.get("DATABASE_NAME"),
+        "ENGINE": os.environ.get(
+            "DATABASE_ENGINE", default="django.db.backends.sqlite3"
+        ),
+        "NAME": os.environ.get("DATABASE_NAME", default="./db.sqlite3"),
         "USER": os.environ.get("DATABASE_USER"),
         "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
         "HOST": os.environ.get("DATABASE_HOST"),
--- config/settings/environment.py
+++ config/settings/environment.py
@@ -8,11 +8,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv(
+SECRET_KEY = os.environ.get(
     "DJANGO_SECRET_KEY", default="qwertyuiopasdfghjkll;kjhgfdsazxcvbnm,mnbvcx"
 )
-DEBUG = os.getenv("DJANGO_DEBUG", default=True)
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", default=["*"])
+DEBUG = os.environ.get("DJANGO_DEBUG", default=True)
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default=["*"])
 ROOT_URLCONF = "config.urls"
 WSGI_APPLICATION = "config.wsgi.application"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
