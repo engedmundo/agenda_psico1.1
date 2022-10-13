ceeac64124f320fbc02227f21d37c1bf4b9ed2d3
Edmundo Abreu e Lima <engedmundo@gmail.com>
2022-10-12 23:14:57 -0300

    update static root

--- config/settings/assets.py
+++ config/settings/assets.py
@@ -5,7 +5,7 @@ from .environment import BASE_DIR
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
 # Extra places for collectstatic to find static files.
