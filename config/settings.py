3cdff00d1a6cceb547c9495e218d5721f1f98dc3
Edmundo Abreu e Lima <engedmundo@gmail.com>
2022-10-22 11:47:49 -0300

    adjust settings

--- config/settings.py
+++ config/settings.py
@@ -17,10 +17,12 @@ environ.Env.read_env()
 SECRET_KEY = env("SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DJANGO_DEBUG")
+# DEBUG = env("DJANGO_DEBUG")
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["34.125.197.120", "psiqueativa.com.br", "127.0.0.1"]
 
+CSRF_TRUSTED_ORIGINS = ["psiqueativa.com.br", "www.psiqueativa.com.br"] 
 
 # Application definition
 
