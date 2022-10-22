c6933f266ed35fc8a98eda0c33758c5295f4a4e9
Edmundo Abreu e Lima <engedmundo@gmail.com>
2022-10-22 16:15:37 -0300

    create a login view

--- apps/front_end/urls/home_urls.py
+++ apps/front_end/urls/home_urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
-from ..views import home, professional_description
+from ..views import (
+    home,
+    login_create,
+    login_view,
+    logout_view,
+    professional_description,
+)
 
 app_name = "site_interface"
 
@@ -15,5 +21,20 @@ urlpatterns = [
         "professional_description/<int:id>",
         professional_description,
         name="professional_description",
+    ),
+    path(
+        "login",
+        login_view,
+        name="login",
+    ),
+    path(
+        "login/create",
+        login_create,
+        name="login_create",
+    ),
+    path(
+        "logout",
+        logout_view,
+        name="logout_view",
     ),
 ]
---
+++ apps/front_end/views/__init__.py
@@ -0,0 +1,7 @@
+from .home_views import (
+    home,
+    login_create,
+    login_view,
+    logout_view,
+    professional_description,
+)
