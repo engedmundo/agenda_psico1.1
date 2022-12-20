02688d714c7a171f822c9c7e3c5aee8a9d30a8db
Edmundo Abreu e Lima <engedmundo@gmail.com>
2022-12-20 09:34:12 -0300

    fix on created_at field

--- apps/core/models/common_info.py
+++ apps/core/models/common_info.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class CommonInfo(models.Model):
-    created_at = models.DateField("Data de criação", auto_now=True)
+    created_at = models.DateField("Data de criação", auto_now_add=True)
     updated_at = models.DateField("Data de atualização", auto_now=True)
     is_active = models.BooleanField("Ativo", default=True)
 
